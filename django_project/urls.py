cd8249a243d336bbcd39b9828a1ac1153e76565c
jstran9 <wptran58@gmail.com>
2020-02-27 14:30:26 -0800

    creating routes for the blog application.

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # if we go to the path of "blog" we be mapping to our urls.py module.
+    # for example: if we go to "blog/" then we will currently be mapping to the home function in our views from the blog app.
+    # note that if do something like "blog/home" then when we forward this request into the "urls.py" file from the blogs app then we will be looking for a path to match "/home" because we have already matched the "blog" portion.
+    # by default even if we don't have a trailing forward slash then Django will re-direct routes without a trailing forward slash to the ones with a trailing forward slash.
+    # path('blog/', include('blog.urls')), # make the blog require a different route.
+    path('', include('blog.urls')),  # make our blog application our home page.
 ]
