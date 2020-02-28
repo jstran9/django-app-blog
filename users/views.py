9ed8f367545ab3591309325972af98e3945fb984
jstran9 <wptran58@gmail.com>
2020-02-28 13:36:46 -0800

    adding logic to display prepopulated fields.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages  # flash message to show we received valid data.
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -23,5 +23,18 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    user = request.user
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Your account has been updated!")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=user)
+        p_form = ProfileUpdateForm(instance=user.profile)
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "users/profile.html", context)
 
