4dc6cc4fc051081066123cd3822aa067cfd809b5
jstran9 <wptran58@gmail.com>
2020-02-28 00:10:37 -0800

    adding a registration route.

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # if we go to the path of "blog" we be mapping to our urls.py module.
     # for example: if we go to "blog/" then we will currently be mapping to the home function in our views from the blog app.
     # note that if do something like "blog/home" then when we forward this request into the "urls.py" file from the blogs app then we will be looking for a path to match "/home" because we have already matched the "blog" portion.
     # by default even if we don't have a trailing forward slash then Django will re-direct routes without a trailing forward slash to the ones with a trailing forward slash.
     # path('blog/', include('blog.urls')), # make the blog require a different route.
-    path('', include('blog.urls')),  # make our blog application our home page.
+    path("", include("blog.urls")),  # make our blog application our home page.
+    path("register/", user_views.register, name="register"),
 ]
