b4f04d75b4a06b38a342bdc0b0dab43ad9da0d73
jstran9 <wptran58@gmail.com>
2020-02-27 17:30:18 -0800

    grabbing data from the database.

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        "author": "CoreyMS",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "August 27, 2018",
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "Second post content",
-        "date_posted": "August 28 2018",
-    },
-]
+from .models import Post
 
 
 def home(request):
     # third argument is optional and is the context which is a way for us to pass information into our template.
     # an http response is returned.
     # our views need to return an http response or an exception.
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
