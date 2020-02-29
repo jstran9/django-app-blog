61b6bc6078eeff9578488a1645837e01063b9de1
jstran9 <wptran58@gmail.com>
2020-02-28 18:20:36 -0800

    creating class based views for create, update, and delete.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def home(request):
@@ -8,6 +16,58 @@ def home(request):
     # our views need to return an http response or an exception.
     context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    # set a new template.
+    template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
+    # set the context's objects.
+    context_object_name = "posts"
+    ordering = ["date_posted"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        # before the form is submitted take the instance of the form and set its author to the logged in author.
+        form.instance.author = self.request.user
+        # run the parent form_valid after setting the author.
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        # before the form is submitted take the instance of the form and set its author to the logged in author.
+        form.instance.author = self.request.user
+        # run the parent form_valid after setting the author.
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
