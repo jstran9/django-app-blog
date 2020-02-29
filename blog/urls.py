8050236a58ba70babc0651e31344b74a86163728
jstran9 <wptran58@gmail.com>
2020-02-28 19:38:47 -0800

    creating route to view posts by a specific user.

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,13 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 
 urlpatterns = [
     # if we want to do a reverse look-up and having a unique name can help us perform this look-up.
     path("", PostListView.as_view(), name="blog-home"),
+    path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
