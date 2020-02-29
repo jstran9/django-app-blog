fd47a0e5128b85982b172847f4042e2d2fd36568
jstran9 <wptran58@gmail.com>
2020-02-28 18:20:12 -0800

    adding routes for create, update, and delete posts.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views  # from the current directory just import the views module.
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
     # if we want to do a reverse look-up and having a unique name can help us perform this look-up.
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path("", PostListView.as_view(), name="blog-home"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("about/", views.about, name="blog-about"),
 ]
