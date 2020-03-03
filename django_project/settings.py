771d6e3cb71d25af75d51df782114361be4d6e47
jstran9 <wptran58@gmail.com>
2020-03-02 18:39:00 -0800

    adding in necessary configuration for AWS s3 support.

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,10 @@ EMAIL_PORT = os.environ["DJANGO_APP_EMAIL_PORT"]
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ["DJANGO_APP_TEST_GMAIL_USER"]
 EMAIL_HOST_PASSWORD = os.environ["DJANGO_APP_TEST_GMAIL_PASSWORD"]
+
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
+AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_DEFAULT_ACL = None
