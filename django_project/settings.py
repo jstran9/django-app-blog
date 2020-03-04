ddf613ef25287316b9c060dbe60997eb33496c85
jstran9 <wptran58@gmail.com>
2020-03-04 15:58:55 -0800

    trying configuration to resolve collectstatic error

--- django_project/settings.py
+++ django_project/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "PROJECT_ROOT")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
