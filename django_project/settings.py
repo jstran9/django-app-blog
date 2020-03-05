6c98817c615ec3cec65db37dcdb9fa6ad18acc9a
jstran9 <wptran58@gmail.com>
2020-03-04 16:52:39 -0800

    extra static configuration.

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,8 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # required for production.
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # change setting to use a different bootstrap style than the default (bootstrap 2)
 CRISPY_TEMPLATE_PACK = "bootstrap4"
