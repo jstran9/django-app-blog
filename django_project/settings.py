b37c60f696b8cde8d13f0e562694cf8d67344fac
jstran9 <wptran58@gmail.com>
2020-03-04 16:51:18 -0800

    adding comment.

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # required for production.
 STATIC_URL = "/static/"
 
 # change setting to use a different bootstrap style than the default (bootstrap 2)
