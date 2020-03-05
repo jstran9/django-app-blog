5951774ebd7565aa2e4c3d10cd460a3b205fe29f
jstran9 <wptran58@gmail.com>
2020-03-04 17:20:40 -0800

    adding allowed host.

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ["DJANGO_APP_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://tt-django-blog-app.herokuapp.com/"]
 
 
 # Application definition
