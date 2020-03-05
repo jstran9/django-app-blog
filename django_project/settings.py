539724299e8533955794607fe7d8ac9a6a33901f
jstran9 <wptran58@gmail.com>
2020-03-04 16:00:38 -0800

    reverting configuration back

--- django_project/settings.py
+++ django_project/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -124,13 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "PROJECT_ROOT")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    "/var/www/static/",
-]
 
 # change setting to use a different bootstrap style than the default (bootstrap 2)
 CRISPY_TEMPLATE_PACK = "bootstrap4"
