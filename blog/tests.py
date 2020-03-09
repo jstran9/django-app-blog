36979a0b4bf13b3b1d0743c02e3213b319f5baa3
todd <wptran58@gmail.com>
2020-03-09 15:44:49 -0700

    testing CRUD functionality and for expected elements on the home page.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,167 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Post
+from django.contrib.auth.models import User
+from django_project.utility import TestDataGenerator
+from django.http import HttpResponse
+from django.http.response import (
+    HttpResponseBase,
+    HttpResponseRedirect,
+    HttpResponseNotFound,
+)
 
-# Create your tests here.
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+
+class PostTestCase(TestCase, TestDataGenerator):
+    def setUp(self):
+        super().createSampleData()
+
+    def test_posts_have_title(self):
+        post_one = Post.objects.get(title=self.TEST_TITLE_ONE)
+        post_two = Post.objects.get(title=self.TEST_TITLE_TWO)
+        self.assertEqual(post_one.title, self.TEST_TITLE_ONE)
+        self.assertEqual(post_two.title, self.TEST_TITLE_TWO)
+
+    def test_posts_have_content(self):
+        post_one = Post.objects.get(title=self.TEST_TITLE_ONE)
+        post_two = Post.objects.get(title=self.TEST_TITLE_TWO)
+        self.assertEqual(post_one.content, self.TEST_CONTENT_ONE)
+        self.assertEqual(post_two.content, self.TEST_CONTENT_TWO)
+
+    def test_posts_has_author(self):
+        user_one = User.objects.get(username=self.TEST_USERNAME_ONE)
+        user_two = User.objects.get(username=self.TEST_USERNAME_TWO)
+        post_one = Post.objects.get(author=user_one)
+        post_two = Post.objects.get(author=user_two)
+        self.assertEqual(post_one.author, user_one)
+        self.assertEqual(post_two.author, user_two)
+
+    def test_get_single_post(self):
+        c = Client()
+        user = User.objects.filter(username=self.TEST_USERNAME_ONE).first()
+        post = Post.objects.filter(author=user).first()
+        response = c.get(f"/post/{post.id}/")
+        self.assertEquals(response.status_code, HttpResponseBase.status_code)
+
+    def test_get_nonexistant_post(self):
+        c = Client()
+        response = c.get(f"/post/0/")
+        self.assertEquals(response.status_code, HttpResponseNotFound.status_code)
+
+    def test_create_post_logged_in(self):
+        c = Client()
+        c.login(username=self.TEST_USERNAME_ONE, password=self.TEST_PASSWORD)
+        post_count = Post.objects.count()
+        self.assertEquals(self.TEST_EXPECTED_SIZE_TWO, post_count)
+        c.post(
+            "/post/new/", {"title": "Test Title", "content": "Test Content"},
+        )
+        new_post_count = Post.objects.count()
+        self.assertNotEquals(post_count, new_post_count)
+        self.assertEquals(self.TEST_EXPECTED_SIZE_THREE, new_post_count)
+
+    def test_create_post_not_logged_in(self):
+        c = Client()
+        post_count = Post.objects.count()
+        self.assertEquals(self.TEST_EXPECTED_SIZE_TWO, post_count)
+        c.post(
+            "/post/new/", {"title": "Test Title", "content": "Test Content"},
+        )
+        new_post_count = Post.objects.count()
+        self.assertEquals(self.TEST_EXPECTED_SIZE_TWO, new_post_count)
+
+    def test_delete_post_logged_in(self):
+        c = Client()
+        c.login(username=self.TEST_USERNAME_ONE, password=self.TEST_PASSWORD)
+        user = User.objects.filter(username=self.TEST_USERNAME_ONE).first()
+        post_count = Post.objects.count()
+        self.assertEquals(self.TEST_EXPECTED_SIZE_TWO, post_count)
+        post = Post.objects.filter(author=user).first()
+        c.delete(f"/post/{post.id}/delete/")
+        new_post_count = Post.objects.count()
+        self.assertNotEquals(post_count, new_post_count)
+        self.assertEquals(self.TEST_EXPECTED_SIZE_ONE, new_post_count)
+
+    def test_delete_post_not_logged_in(self):
+        c = Client()
+        user = User.objects.filter(username=self.TEST_USERNAME_ONE).first()
+        post_count = Post.objects.count()
+        self.assertEquals(self.TEST_EXPECTED_SIZE_TWO, post_count)
+        post = Post.objects.filter(author=user).first()
+        c.delete(f"/post/{post.id}/delete/")
+        new_post_count = Post.objects.count()
+        self.assertEquals(self.TEST_EXPECTED_SIZE_TWO, new_post_count)
+
+    def test_update_post_logged_in(self):
+        c = Client()
+        c.login(username=self.TEST_USERNAME_ONE, password=self.TEST_PASSWORD)
+        user = User.objects.filter(username=self.TEST_USERNAME_ONE).first()
+        post = Post.objects.filter(author=user).first()
+        new_title = f"{post.title}123"
+        content = {"title": new_title, "content": post.content}
+        c.post(f"/post/{post.id}/update/", content)
+        updated_post = Post.objects.filter(id=post.id).first()
+        self.assertEquals(new_title, updated_post.title)
+
+    def test_update_post_not_logged_in(self):
+        c = Client()
+        user = User.objects.filter(username=self.TEST_USERNAME_ONE).first()
+        post = Post.objects.filter(author=user).first()
+        new_title = f"{post.title}123"
+        content = {"title": new_title, "content": post.content}
+        c.post(f"/post/{post.id}/update/", content)
+        updated_post = Post.objects.filter(id=post.id).first()
+        self.assertNotEquals(new_title, updated_post.title)
+
+
+class UserViewTestCase(TestCase, TestDataGenerator):
+    def setUp(self):
+        super().createSampleData()
+
+    def test_view_profile_as_logged_in(self):
+        c = Client()
+        c.login(username=self.TEST_USERNAME_ONE, password=self.TEST_PASSWORD)
+        response = c.get("/profile/")
+        self.assertEqual(HttpResponseBase.status_code, response.status_code)
+
+    def test_view_profile_as_not_logged_in(self):
+        c = Client()
+        response = c.get("/profile/")
+        self.assertEqual(HttpResponseRedirect.status_code, response.status_code)
+
+
+# TODO: will need to rename this once I write a few tests using it.
+class MySeleniumTests(StaticLiveServerTestCase, TestDataGenerator):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        super().createSampleData()
+
+    def login_helper(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys(self.TEST_USERNAME_ONE)
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys(self.TEST_PASSWORD)
+        self.selenium.find_element_by_xpath('/html/body/main/div/div[1]/div/form/div/button').click()
+
+    def test_navbar_content(self):
+        self.login_helper()
+        profile_link = self.selenium.find_element_by_xpath('//*[@id="navbarToggle"]/div[2]/a[2]')
+        self.assertEqual(profile_link.text, "Profile")
+        new_post_link = self.selenium.find_element_by_xpath('/html/body/header/nav/div/div/div[2]/a[1]')
+        self.assertEqual(new_post_link.text, "New Post")
+        log_out_link = self.selenium.find_element_by_xpath('/html/body/header/nav/div/div/div[2]/a[3]')
+        self.assertEqual(log_out_link.text, "Logout")
